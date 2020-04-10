4526bff25900cf608fc3175399f7542424faa338
Anubhav Bhardwaj <61362008+abd8126@users.noreply.github.com>
2020-04-10 21:15:38 +0530

    Add files via upload
    Stagr 2 Completed

---
+++ stage2/project/Main.java
@@ -0,0 +1,12 @@
+class MyClass {
+    public static void main(String args[]) {
+        
+        int a = 15;
+        int b = 14;
+        int c = 14;
+        
+        boolean result = (a >= b) && (b != c);
+        
+        System.out.println(result);
+    }
+}
